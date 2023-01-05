c6292049bf430938e340ebdcafb90818a4d612c9
michaelolof <myqrrlolof@yahoo.com>
2023-01-05 19:43:06 +0100

    feat: initialized repo with eslint

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+import eslintPlugin from "vite-plugin-eslint";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    vite: {
+        plugins: [
+            eslintPlugin({
+                emitWarning: true,
+                emitError: true,
+            }),
+        ],
+    },
+});
