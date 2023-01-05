b4846a7400ddfbf1e470311bc8bfead75a8dbc00
michaelolof <myqrrlolof@yahoo.com>
2023-01-05 20:37:18 +0100

    feat: integrated scss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,26 @@ import eslintPlugin from "vite-plugin-eslint";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+
+    css: [
+        "@/assets/scss/main.scss",
+    ],
+
     modules: [
         "@pinia/nuxt",
     ],
 
     vite: {
+        css: {
+            preprocessorOptions: {
+                sccs: {
+                    additionalData: `
+                        @import '@/assets/scss/abstracts/variables.scss'
+                    `,
+                },
+            },
+        },
+
         plugins: [
             eslintPlugin({
                 emitWarning: true,
